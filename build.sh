8b303f296cf285290ef890504fa05c17fe19c744
Jacob <kingsleynuaa@gmail.com>
2016-08-23 11:51:17 +0800

    delete cuda

--- build.sh
+++ build.sh
@@ -2,29 +2,13 @@
 mkdir deps &> /dev/null
 cd deps
 
-#Add necessary extra repos
-version=$(lsb_release -a 2>&1)
-if [[ $version == *"14.04"* ]] ; then
-    wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_7.5-18_amd64.deb
-    sudo dpkg -i cuda-repo-ubuntu1404_7.5-18_amd64.deb
-    #rm cuda-repo-ubuntu1404_7.5-18_amd64.deb
-    sudo add-apt-repository -y ppa:v-launchpad-jochen-sprickerhof-de/pcl
-    sudo apt-get update
-    sudo apt-get install -y libpcl-all
-elif [[ $version == *"15.04"* ]] ; then
-    wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1504/x86_64/cuda-repo-ubuntu1504_7.5-18_amd64.deb
-    sudo dpkg -i cuda-repo-ubuntu1504_7.5-18_amd64.deb
-    #rm cuda-repo-ubuntu1504_7.5-18_amd64.deb
-    sudo apt-get update
-    sudo apt-get install -y libpcl-dev yasm libvtk5-qt4-dev
-else
-    echo "Don't use this on anything except 14.04 or 15.04"
-    exit
-fi
 
 sudo apt-get install -y libeigen3-dev libboost1.55-dev cmake-qt-gui git build-essential libusb-1.0-0-dev libudev-dev openjdk-7-jdk freeglut3-dev python-vtk libvtk-java libglew-dev cuda-7-5 libsuitesparse-dev libvtk6-dev libqhull-dev
 sudo apt-get install cmake
 
+sudo apt-get install -y cmake-qt-gui git build-essential libusb-1.0-0-dev libudev-dev openjdk-7-jdk freeglut3-dev python-vtk libvtk-java libglew-dev libsuitesparse-dev
+
+#Build FLANN
 wget http://www.cs.ubc.ca/research/flann/uploads/FLANN/flann-1.8.4-src.zip
 unzip flann-1.8.4-src.zip
 cd flann-1.8.4-src
@@ -49,7 +33,7 @@ cd ../Redist/OpenNI-Bin-Dev-Linux-x64-v1.5.7.10
 sudo ./install.sh
 cd ../../../../../
 
-sudo apt-get install -y cmake-qt-gui git build-essential libusb-1.0-0-dev libudev-dev openjdk-7-jdk freeglut3-dev python-vtk libvtk-java libglew-dev libsuitesparse-dev
+
 
 
 
@@ -71,6 +55,7 @@ source /etc/bash.bashrc
 cd ../..
 
 
+#Build PCL from source
 wget https://github.com/PointCloudLibrary/pcl/archive/pcl-1.6.0.tar.gz
 tar -xzvf pcl-1.6.0.tar.gz
 cd pcl-pcl-1.6.0

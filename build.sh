8f69ae2c04bca6dd8ae48bb810df66f51ab691d6
Jacob <kingsleynuaa@gmail.com>
2016-08-23 11:34:36 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -22,25 +22,32 @@ else
     exit
 fi
 
-sudo apt-get install -y libeigen3-dev libboost1.55-dev cmake-qt-gui git build-essential libusb-1.0-0-dev libudev-dev openjdk-7-jdk freeglut3-dev python-vtk libvtk-java libglew-dev cuda-7-5 libsuitesparse-dev
+sudo apt-get install -y libeigen3-dev libboost1.55-dev cmake-qt-gui git build-essential libusb-1.0-0-dev libudev-dev openjdk-7-jdk freeglut3-dev python-vtk libvtk-java libglew-dev cuda-7-5 libsuitesparse-dev libvtk6-dev libqhull-dev
+sudo apt-get install cmake
 
 wget http://www.cs.ubc.ca/research/flann/uploads/FLANN/flann-1.8.4-src.zip
 unzip flann-1.8.4-src.zip
+cd flann-1.8.4-src
+
 mkdir build
 cd build
 cmake ..
 make
 sudo make install
 
-sudo apt-get install libvtk6-dev libqhull-dev
+cd ../..
+
+
 
 # Build OpenNI1
 sudo apt-get install libusb-1.0-0-dev freeglut3-dev openjdk-7-jdk doxygen graphviz mono-complete
 git clone https://github.com/OpenNI/OpenNI
-cd Platform/Linux/CreateRedist/
+cd OpenNI/Platform/Linux/CreateRedist/
 ./RedisMaker
+cd 
 cd ../Redist/OpenNI-Bin-Dev-Linux-x64-v1.5.7.10
 sudo ./install.sh
+cd ../../../../../
 
 sudo apt-get install -y cmake-qt-gui git build-essential libusb-1.0-0-dev libudev-dev openjdk-7-jdk freeglut3-dev python-vtk libvtk-java libglew-dev libsuitesparse-dev
 
@@ -66,7 +73,7 @@ cd ../..
 
 wget https://github.com/PointCloudLibrary/pcl/archive/pcl-1.6.0.tar.gz
 tar -xzvf pcl-1.6.0.tar.gz
-cd pcl-1.6.0
+cd pcl-pcl-1.6.0
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release .. 
